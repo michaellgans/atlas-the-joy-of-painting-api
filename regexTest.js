9e4f6cfcd8c88b53fc88bc12ea66a48bd737baaa
michaellgans <michaellgans@Victoria>
2024-07-29 22:13:08 -0500

    M - 10:12 PM - successfully reads data from txt file and matches date and painting title with a better regex pattern.

--- regexTest.js
+++ regexTest.js
@@ -1,5 +1,7 @@
 // Exploring Regex Patterns in a JavaScript File
 
+const fs = require('fs');
+
 function pullValues() {
     // String to search through
     let str = '"A Walk in the Woods" (January 11, 1983)';
@@ -51,6 +53,45 @@ function pullValues2() {
     };
 }
 
-// pullValues();
-// console.log("-----")
+function pullValuesFromFile() {
+    // Pulls lines from file instead of from hardcode
+    // Will print"No match found" if there is a new line
+    // at the end of the txt file!
+    const filePath = './testing/testText.txt';
+
+    let str;
+    // Try to read contents of file
+    try {
+        str = fs.readFileSync(filePath, 'utf8');
+        console.log("File has been read!");
+    } catch (err) {
+        console.error(err);
+        return;
+    }
+
+    let csvLines = str.split(/\n/);
+
+    // Regex pattern (finds two matches)
+    // Find any number of characters that aren't " between two ".
+    // Finds the first "word" after the first (
+    let regexPatterns = /"([^"]*)"\s+\((\w+)/;
+
+    for (let line of csvLines) {
+        let match = line.match(regexPatterns);
+        // check for null
+        if (match) {
+            let paintingTitle = match[1];
+            let date = match[2];
+            console.log(`Title: ${paintingTitle}. Date: ${date}`);
+        } else {
+            console.log("No match found");
+        }
+    };
+}
+
+// Tests
+pullValues();
+console.log("-----")
 pullValues2();
+console.log("-----")
+pullValuesFromFile();
