04bbfe2631892a9bd4ccab20a3ab19adf85507e3
michaellgans <michaellgans@Victoria>
2024-07-29 21:29:26 -0500

    M - 9:29 PM - creates pullValues() to pull title and date from episode_dates.txt

--- regexTest.js
+++ regexTest.js
@@ -1,12 +1,28 @@
 // Exploring Regex Patterns in a JavaScript File
 
-// String to search through
-let str = '"A Walk in the Woods" (January 11, 1983)';
-// Regex patterns
-let paintingTitle = /"([^"]*)"/;
-let date = /\(([^ ]*)/ ;
-let titleResult = str.match(paintingTitle);
-let dateResult = str.match(date);
+function pullValues() {
+    // String to search through
+    let str = '"A Walk in the Woods" (January 11, 1983)';
 
-console.log(titleResult[1]);
-console.log(dateResult[1]);
+    // Regex patterns
+    // Find any number of characters that aren't " between two ".
+    let paintingTitle = /"([^"]*)"/;
+    // Finds the first "word" after the first (
+    let date = /\((\w+)/ ;
+    let titleResult = str.match(paintingTitle);
+    let dateResult = str.match(date);
+
+    if (titleResult && titleResult[1]) {
+        console.log(titleResult[1]);
+    } else {
+        console.log("Couldn't match title.");
+    }
+
+    if (dateResult && dateResult[1]) {
+        console.log(dateResult[1]); 
+    } else {
+        console.log("Couldn't match date.");
+    }
+}
+
+pullValues();
