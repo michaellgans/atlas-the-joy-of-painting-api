38cb0780787096197dfbac35bbed04368cf7a26a
michaellgans <michaellgans@Victoria>
2024-07-31 12:52:11 -0500

    M - testing media script

--- createCSVScripts/media.js
+++ createCSVScripts/media.js
@@ -10,8 +10,8 @@ const writeFileUtil = require('../utils/writeFileUtil.js');
 
 function regexColorsUsed() {
     // Read source CSV file
-    // const txtLines = readFileUtil('./sources/subject_matter.csv');
-    const txtLines = readFileUtil('./testing/testColorsUsed.csv');
+    const txtLines = readFileUtil('./sources/subject_matter.csv');
+    // const txtLines = readFileUtil('./testing/testColorsUsed.csv');
 
     // Store title string
     const csvHeader = txtLines[0]; // Origional Header
@@ -22,13 +22,11 @@ function regexColorsUsed() {
 
     // Define start of new header
     // TODO: CHANGE THIS DEPENDING ON WHAT YOU'RE PRINTING!!!
-    let newCSVHeader = "id,painting_title,season_helper,episode_helper,total_colors,youtube_src,"
-    newCSVHeader = newCSVHeader + paintingColors;
+    let newCSVHeader = "id,painting_title,youtube_src"
     let formattedHeader = capWordUtil(newCSVHeader, ",");
 
     // Create clean dictionary with data needed
     const regexPatterns = /^(\d*),\d*,([^,]*),([^,]*),(\d),(\d),(\d),([^,]*)(,"([^"]*")){2}/;
-    // const match = txtLines[1].match(regexPatterns);
 
     // console.log(`id = ${match[1]}, paintingTitle = ${match[3]}, season_helper = ${match[4]}, episode_helper = ${match[5]}, total_colors = ${match[6]}, youtube_src = ${match[7]}`);
     const dict = new Map();
@@ -58,8 +56,8 @@ function regexColorsUsed() {
         }
     });
 
-    console.log(`Size of dictionary: ${dict.size}`);
-    console.log(dict);
+    // console.log(`Size of dictionary: ${dict.size}`);
+    // console.log(dict);
 
     // Create new CSV with:
     // id, title, youtube_src
@@ -67,13 +65,14 @@ function regexColorsUsed() {
 
     // Transform Map into Array
     // TODO: CHANGE THIS DEPENDING ON WHAT YOU'RE PRINTING!!!
-    // data = [...dict.entries()].map(([id, { newPaintingTitle, paintingSubjects }]) => [
-    //     id,
-    //     newPaintingTitle,
-    //     paintingSubjects
-    // ]);
+    data = [...dict.entries()].map(([id, { paintingTitle, youtube_src }]) => [
+        id,
+        paintingTitle,
+        youtube_src
+    ]);
 
-    // writeFileUtil('./subject.csv', headersArray, data);
+    writeFileUtil('./transformedCSVs/media.csv', headersArray, data);
+    // writeFileUtil('./testing/testMedia.csv', headersArray, data);
 }
 
 // Tests
