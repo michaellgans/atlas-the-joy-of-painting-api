aee439a091f87ce9bf4236e4de4acfe3f3803ed5
michaellgans <michaellgans@Victoria>
2024-07-30 21:21:46 -0500

    M - 9:21 PM - adds headers to writeFileUtil.js

--- utils/writeFileUtil.js
+++ utils/writeFileUtil.js
@@ -2,9 +2,9 @@
 
 const fs = require('fs');
 
-function writeFileUtil(filePath, data) {
+function writeFileUtil(filePath, headers, data) {
     // Convert data to CSV format
-    const csvLines = data.map(row => row.join(',')).join('\n');
+    const csvLines = [headers, ...data].map(row => row.join(',')).join('\n');
 
     fs.writeFile(filePath, csvLines, 'utf-8', (err) => {
         if (err) {
@@ -15,14 +15,14 @@ function writeFileUtil(filePath, data) {
     });
 }
 
-const filePath = './test.csv';
-const data = [
-    ['ID', 'Title', 'Season'],
-    [1, 'Quiet Woods', 'Fall'],
-    [2, 'Mountain Peaks', 'Winter'],
-    [3, 'Sunny Meadow', 'Spring']
-];
+// const filePath = './test.csv';
+// const data = [
+//     [1, 'Quiet Woods', 'Fall'],
+//     [2, 'Mountain Peaks', 'Winter'],
+//     [3, 'Sunny Meadow', 'Spring']
+// ];
+// const headers = ['ID', 'Title', 'Season'];
 
-writeFileUtil(filePath, data);
+// writeFileUtil(filePath, headers, data);
 
-// module.exports = writeFileUtil;
+module.exports = writeFileUtil;
