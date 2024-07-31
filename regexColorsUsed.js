99838eaa2107e14c451451acdb41f8567cbc5bed
michaellgans <michaellgans@Victoria>
2024-07-31 09:45:12 -0500

    M - 9:44 AM - updates regexColorsUsed with todo comments

--- regexColorsUsed.js
+++ regexColorsUsed.js
@@ -8,63 +8,68 @@ const writeFileUtil = require('./utils/writeFileUtil.js');
 
 function regexColorsUsed() {
     // Read source CSV file
-    const txtLines = readFileUtil('./sources/subject_matter.csv');
+    // const txtLines = readFileUtil('./sources/subject_matter.csv');
+    const txtLines = readFileUtil('./testing/testColorsUsed.csv');
 
     // Store title string
     const csvHeader = txtLines[0];
-    const regexEpisode = /^[^,]*/; // Matches: "EPISODE"
+    // TODO: 
+    // Isolate title, youtube, season, episode, total_colors, true/false
+    // Change season and episode to season_helper ect
+    // Change num_colors to total_colors?
+    // const regexEpisode = /^[^,]*/; // Matches: "EPISODE"
 
-    // Remove "Episode" from title string and add "id"
-    let newCSVHeader = "ID" + csvHeader.replace(regexEpisode, "");
-    newCSVHeader.toLowerCase();
+    // // Remove "Episode" from title string and add "id"
+    // let newCSVHeader = "ID" + csvHeader.replace(regexEpisode, "");
+    // newCSVHeader.toLowerCase();
 
-    // Create clean dictionary with:
-    // id, title, season, subjects
-    const regexPatterns = /([^,]*),"{3}([^"]*)"{3},/;
+    // // Create clean dictionary with:
+    // // id, title, season, subjects
+    // const regexPatterns = /([^,]*),"{3}([^"]*)"{3},/;
 
-    const dict = new Map();
+    // const dict = new Map();
 
-    // Callback method to loop through each line of text
-    txtLines.forEach((line, index) => {
-        let match = line.match(regexPatterns);
-        // Skips the Header of the CSV 
-        if (index === 0) {
-            return;
-        }
-        if (match) {
-            let paintingSeason = match[1];
+    // // Callback method to loop through each line of text
+    // txtLines.forEach((line, index) => {
+    //     let match = line.match(regexPatterns);
+    //     // Skips the Header of the CSV 
+    //     if (index === 0) {
+    //         return;
+    //     }
+    //     if (match) {
+    //         let paintingSeason = match[1];
 
-            // Transforming Painting Title
-            let paintingTitle = match[2]; // ALL CAPS
-            let newPaintingTitle = paintingTitle.toLowerCase(); // lowercase
-            newPaintingTitle = capWordUtil(newPaintingTitle); // Final Form
+    //         // Transforming Painting Title
+    //         let paintingTitle = match[2]; // ALL CAPS
+    //         let newPaintingTitle = paintingTitle.toLowerCase(); // lowercase
+    //         newPaintingTitle = capWordUtil(newPaintingTitle); // Final Form
 
-            // Capture true/false subject string from each line
-            let paintingSubjects = line.replace(regexPatterns, "");
+    //         // Capture true/false subject string from each line
+    //         let paintingSubjects = line.replace(regexPatterns, "");
 
-            // Adds auto-incrementing index to account for
-            // Map overwriting non-unique keys
-            dict.set(index, { newPaintingTitle, paintingSeason, paintingSubjects });
-        } else {
-            console.log("No pattern found.");
-        }
-    });
+    //         // Adds auto-incrementing index to account for
+    //         // Map overwriting non-unique keys
+    //         dict.set(index, { newPaintingTitle, paintingSeason, paintingSubjects });
+    //     } else {
+    //         console.log("No pattern found.");
+    //     }
+    // });
 
-    console.log(`Size of dictionary: ${dict.size}`);
-    // console.log(dict);
+    // console.log(`Size of dictionary: ${dict.size}`);
+    // // console.log(dict);
 
-    // Create new CSV with:
-    // id, title, string
-    headersArray = newCSVHeader.split(',');
+    // // Create new CSV with:
+    // // id, title, string
+    // headersArray = newCSVHeader.split(',');
 
-    // Transform Map into Array
-    data = [...dict.entries()].map(([id, { newPaintingTitle, paintingSubjects }]) => [
-        id,
-        newPaintingTitle,
-        paintingSubjects
-    ]);
+    // // Transform Map into Array
+    // data = [...dict.entries()].map(([id, { newPaintingTitle, paintingSubjects }]) => [
+    //     id,
+    //     newPaintingTitle,
+    //     paintingSubjects
+    // ]);
 
-    writeFileUtil('./subject.csv', headersArray, data);
+    // writeFileUtil('./subject.csv', headersArray, data);
 }
 
 // Tests
