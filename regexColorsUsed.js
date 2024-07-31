3dd21ceae5014892209b2cdd4265e48223aa63f1
michaellgans <michaellgans@Victoria>
2024-07-31 12:31:52 -0500

    M - 12:31 PM - stores all needed data from Colors Used into Map Dictionary

--- regexColorsUsed.js
+++ regexColorsUsed.js
@@ -26,39 +26,38 @@ function regexColorsUsed() {
 
     // Create clean dictionary with data needed
     const regexPatterns = /^(\d*),\d*,([^,]*),([^,]*),(\d),(\d),(\d),([^,]*)(,"([^"]*")){2}/;
-    const match = txtLines[1].match(regexPatterns);
+    // const match = txtLines[1].match(regexPatterns);
 
-    console.log(`id = ${match[1]}, paintingTitle = ${match[3]}, season_helper = ${match[4]}, episode_helper = ${match[5]}, total_colors = ${match[6]}, youtube_src = ${match[7]}`);
-    // const dict = new Map();
+    // console.log(`id = ${match[1]}, paintingTitle = ${match[3]}, season_helper = ${match[4]}, episode_helper = ${match[5]}, total_colors = ${match[6]}, youtube_src = ${match[7]}`);
+    const dict = new Map();
 
-    // // Callback method to loop through each line of text
-    // txtLines.forEach((line, index) => {
-    //     let match = line.match(regexPatterns);
-    //     // Skips the Header of the CSV 
-    //     if (index === 0) {
-    //         return;
-    //     }
-    //     if (match) {
-    //         let paintingSeason = match[1];
+    // Callback method to loop through each line of text
+    txtLines.forEach((line, index) => {
+        let match = line.match(regexPatterns);
+        // Skips the Header of the CSV 
+        if (index === 0) {
+            return;
+        }
+        if (match) {
+            let id = match[1];
+            let paintingTitle = match[3];
+            let season_helper = match[4];
+            let episode_helper = match[5];
+            let total_colors = match[6];
+            let youtube_src = match[7];
 
-    //         // Transforming Painting Title
-    //         let paintingTitle = match[2]; // ALL CAPS
-    //         let newPaintingTitle = paintingTitle.toLowerCase(); // lowercase
-    //         newPaintingTitle = capWordUtil(newPaintingTitle); // Final Form
+            // Capture true/false color string from each line
+            let colorsData = line.replace(regexPatterns, "");
 
-    //         // Capture true/false subject string from each line
-    //         let paintingSubjects = line.replace(regexPatterns, "");
+            // Adds data from CSV to Map Dictionary
+            dict.set(id, { paintingTitle, season_helper, episode_helper, total_colors, youtube_src, colorsData });
+        } else {
+            console.log("No pattern found.");
+        }
+    });
 
-    //         // Adds auto-incrementing index to account for
-    //         // Map overwriting non-unique keys
-    //         dict.set(index, { newPaintingTitle, paintingSeason, paintingSubjects });
-    //     } else {
-    //         console.log("No pattern found.");
-    //     }
-    // });
-
-    // console.log(`Size of dictionary: ${dict.size}`);
-    // // console.log(dict);
+    console.log(`Size of dictionary: ${dict.size}`);
+    console.log(dict);
 
     // // Create new CSV with:
     // // id, title, string
